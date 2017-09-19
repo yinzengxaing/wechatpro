f8d3a0677f9266d2658c2167fe33acc596861c34
yinzengxiang <709076719@qq.com>
2017-09-19 19:14:02 +0800

    提交更新

--- src/com/ssm/wechatpro/service/impl/EmpowerWebpageServiceImpl.java
+++ src/com/ssm/wechatpro/service/impl/EmpowerWebpageServiceImpl.java
@@ -29,13 +29,16 @@ public class EmpowerWebpageServiceImpl implements EmpowerWebpageService {
 	@Override
 	public void getOpenidBycode(InputObject inputObject,OutputObject outputObject) throws Exception{
 		Map<String ,Object> params = inputObject.getParams();
+		System.out.println("xinrui params:"+params);
 		String code = (String) params.get("code");
 		Map<String,Object> bean = GetOpenIdByCode.getRequest1(Constants.APPID, Constants.APPSECRET, code);
+		System.out.println("xinrui bean:"+bean);
 		if(bean == null){
 			outputObject.setreturnMessage("session为空");
 			return;
 		}
 		Map<String,Object> user = wechatUserMapper.selectWechatUserByOpenId(bean);
+		System.out.println("xinrui user:"+user);
 		outputObject.setWechatLogParams(user);
 		outputObject.setBean(user);
 	}
